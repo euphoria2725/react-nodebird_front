41943a0383ce82fc905ace311132ec041ef5be82
page <euphoria2725@gmail.com>
2023-04-15 01:17:20 +0900

    feat: NextJS와 redux 연동 시 legacy 코드 걷어내기

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Provider } from "react-redux";
 import Head from "next/head";
 import "antd/dist/antd.css";
 
 import wrapper from "../store/configureStore";
 
-const App = ({ Component }) => {
+const App = ({ Component, ...rest }) => {
+  const { store, props } = wrapper.useWrappedStore(rest);
+
   return (
-    <>
+    <Provider store={store}>
       <Head>
         <meta charSet="utf-8" />
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <Component />
-    </>
+    </Provider>
   );
 };
 
 App.propTypes = {
   Component: PropTypes.elementType.isRequired,
+  store: PropTypes.object,
 };
 
-export default wrapper.withRedux(App);
+export default App;
