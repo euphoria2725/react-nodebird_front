27c2b26f6ffc0956daa04c5ab030f91f1c85dcd7
page <euphoria2725@gmail.com>
2023-04-15 18:32:30 +0900

    feat: NextJS WebPack 설정

--- next.config.js
+++ next.config.js
@@ -1,5 +1,22 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
+  compress: true,
+  webpack(config, { webpack }) {
+    const prod = process.env.NODE_ENV === "production";
+    return {
+      ...config,
+      mode: prod ? "production" : "developement",
+      devtool: prod ? "hidden-source-map" : "eval-source-map",
+      plugins: [
+        ...config.plugins,
+        new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
+      ],
+    };
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
-};
+});
